e8ec7e10cd3dfd97c38e089bdaed691b0326a365
unknown <emanuel.molina.marchand@gmail.com>
2022-08-16 11:59:25 -0500

    update labclase 1

---
+++ Laboratorio/Labclase_1.R
@@ -0,0 +1,26 @@
+#Emanuel Molina Marchan
+#09/08/2022
+#clase 1
+
+#srive para realizar comentarios
+#operaciones basicas
+2*2
+
+h <-2
+
+h * h 
+
+g <- c(3, 5, 8, 9) 
+
+g * 2
+
+
+
+
+
+
+
+
+
+
+
